c90bf286f3868d800ee7e13e9a58aee2683d4dc3
Josh Cox <josh@webhosting.coop>
2017-11-16 10:43:59 -0600

    v1.5.8.0-leahlovise

--- client/llc/FooterContainer.jsx
+++ client/llc/FooterContainer.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import { registerComponent } from "/imports/plugins/core/components/lib";
 
 const FooterContainer = () => (
-  <div id="navbar footer-bottom">
+  <div id="navbar" className="footer-center">
 		<div className="outerbox clearfix">
 			<div id="copyright">Copyright 2017</div>
 		</div>
+    <a href="https://leahlovise.com/" title="Leah Lovise" rel="home">
+       www.leahlovise.com
+    </a>
 	</div>
   );
 
